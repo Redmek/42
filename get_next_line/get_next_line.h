dc09fe18b8a79e97add55b371d94ef1ec78d4c72
Redmek <37319944+Redmek@users.noreply.github.com>
2019-12-16 15:08:25 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: remekram <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/06 17:07:23 by remekram          #+#    #+#             */
+/*   Updated: 2019/12/12 20:40:55 by remekram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+int		get_next_line(int fd, char **line);
+
+#endif
